0022f7aaecdfb7170d732729082ac463cb5d450e
hishamlweis <67555212+hishamlweis@users.noreply.github.com>
2020-09-04 22:56:41 +0300

    Add files via upload

---
+++ colors-app/build/precache-manifest.85da8080e85e4dc715fb1d20c3ee9fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9d686ded00365ef49f188b85ba8a52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67525b3f69ea94be2551",
+    "url": "/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "424d9baa6379007a3776",
+    "url": "/static/css/main.457446de.chunk.css"
+  },
+  {
+    "revision": "67525b3f69ea94be2551",
+    "url": "/static/js/2.aa047071.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/static/js/2.aa047071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424d9baa6379007a3776",
+    "url": "/static/js/main.8c68c321.chunk.js"
+  },
+  {
+    "revision": "22cf7d7426dc07f57b93",
+    "url": "/static/js/runtime-main.46079b1d.js"
+  }
+]);
